1f03dafefefade41766f1b5049c95533fd77c6b9
Bertrand Steinmetz <bstenm@hotmail.com>
2019-10-31 11:45:53 -0400

    added styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Button from 'react-bootstrap/Button';
 import './App.css';
 
 const App: React.FC<{ count: number }> = props => {
   const [count, setCount] = useState(0);
 
+  const StyledButton = styled(Button)`
+    margin-bottom: 20px;
+  `;
+
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant="light" onClick={() => setCount(count + 1)}>
+        <StyledButton variant="light" onClick={() => setCount(count + 1)}>
           Add
-        </Button>
+        </StyledButton>
         <p>Count: {count}</p>
       </header>
     </div>
